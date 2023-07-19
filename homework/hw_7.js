db53af28e82b5c4a575a1fa14be15d05d956ea7f
TotSamiy2Pac <a.r.a.p.o.v@yandex.ru>
2023-07-19 19:57:29 +0600

    HomeWork_7

--- homework/hw_7.js
+++ homework/hw_7.js
@@ -36,3 +36,24 @@
 // }
 //
 // console.log(arrSum([1, 2, 5, 9, 3, 13, 2, 10]))
+
+
+function sumArray(array) {
+    let sumMaxMin = -(Math.max.apply(null, array) + Math.min.apply(null, array))
+    if (array !== null && array !== [] && array){
+        // console.log('yes')
+        if (array.length > 1) {
+            return array.reduce((acc, el) => {
+                return acc += el
+            }, sumMaxMin)
+        }
+        else {
+            return 0
+        }
+    }
+    else {
+        return 0
+    }
+}
+
+console.log(sumArray([ 0 ]))
